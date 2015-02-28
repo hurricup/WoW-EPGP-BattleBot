5ff3ac9f00031558fd3156d0b305b5b673c430f0
hurricup <hurricup@gmail.com>
2015-02-28 17:59:41 +0300

    Marked buff event as NYI Updated configuration loading and cleanup Added dependency from epgp addon Updated english locale

--- README.md
+++ README.md
@@ -19,10 +19,11 @@ Addon configuration may be managed via `/epgpbb` slash commands:
 * `/epgpbb add GP_value on event` - add new rule to charge GP on event. Events are:
   * `death by spell_id` - charge GP on taking damage with overkill from spell by id
   * `damage by spell_id` - charge GP on taking damage from spell by id
-  * `buff spell_id [stacks min_staks]` - charge GP to players, gathered min_staks of buff/debuff
+  * `buff spell_id [stacks min_staks]` - charge GP to players, gathered min_staks of buff/debuff (NYI)
 * `/epgpbb del rule_id` - delete rule by id
 * `/epgpbb enable rule_id` - enable rule by id
 * `/epgpbb disable rule_id` - disable rule by id
+* `/epgpbb reset` - reset configuration
 
 #LINKS
 * Repository: https://github.com/hurricup/WoW-EPGP-BattleBot
--- epgp_battle_bot/battle_bot.lua
+++ epgp_battle_bot/battle_bot.lua
@@ -1,33 +1,119 @@
 local version = "6.1"
 local cfg_death
-local cfg_damage
+local cfg_damage_done
+local cfg_damage_taken
 local cfg_buff
+
+local config_keys = {
+    "death",
+    "damage_done",
+    "damage_taken",
+    "buff",
+};
+
+function battle_bot_check_config_keys()
+    for _, key in pairs(config_keys) do
+        if( not config[key] ) then
+            config[key] = {}
+        end
+    end
+end
+
+function battle_bot_reset_handler()
+    config = {}
+    battle_bot_check_config_keys()
+    print(EPGP_BB_CONFIG_RESET)
+end
+
+function battle_bot_add_handler( cmd, tail )                           
+    _, _, gp_value, action_base, action_ext, actor, comment = string.find( tail, '^(%d+)%s+on%s+(%a+)%s+(%a+)%s+(%w+)%s*(.*)$' )
+    
+    -- /ebb add 123 on damage by 123 Попал под поезд
+    if( action_base == 'damagedone' ) then
+        if( action_ext == 'by' ) then
+            local item = {
+                ['type'] = 'spellid',
+                ['spellid'] = actor,
+            }
+            battle_bot_add_rule( 'damage', item, comment )
+        else
+            battle_bot_help_handler();
+        end
+    -- /ebb add 123 on death by 123 Попал под поезд
+    elseif( action_base == 'death' ) then
+        if( action_ext == 'by' ) then
+            item = {
+                ['type'] = 'spellid',
+                ['spellid'] = actor,
+            }
+            battle_bot_add_rule( 'death', item, comment )
+        else
+            battle_bot_help_handler();
+        end
+    else
+        battle_bot_help_handler();
+    end
+end
+
+function battle_bot_list_handler( cmd, tail )
+    print( cmd, tail);
+end
+
+function battle_bot_del_handler( cmd, tail )
+    print( cmd, tail);
+end
+
+function battle_bot_enable_handler( cmd, tail )
+    print( cmd, tail);
+end
+
+function battle_bot_disable_handler( cmd, tail )
+    print( cmd, tail);
+end
+
+function battle_bot_help_handler()
+    print(EPGP_BB_HELP:format(version))
+end
+
+local slash_handlers = {
+    list    = battle_bot_list_handler,
+    add     = battle_bot_add_handler,
+    del     = battle_bot_del_handler,
+    enable  = battle_bot_enable_handler,
+    disable = battle_bot_disable_handler,
+    reset   = battle_bot_reset_handler,
+}
 
 function battle_bot_slash_handler( msg, box)
     msg = string.lower(msg)
 
-    if( not msg or msg == "" or msg == "help" ) then
-        print(EPGP_BB_HELP:format(version))
---    elseif( msg == "list" ) then
+    _, _, cmd, tail = string.find( msg, '^%s*(%a+)%s*(.*)$');
+    
+    if( slash_handlers[cmd] ) then
+        slash_handlers[cmd](cmd, tail)
     else
-        print("Got "..msg)
+        battle_bot_help_handler();
     end
 end
 
-function battle_bot_init()
+function battle_bot_validate_config()
     if( not config) then
-        config = {
-            ["death"] = {},
-            ["damage"] = {},
-            ["buff"] = {}
-        }
+        battle_bot_reset_handler()
     end
 
+    battle_bot_check_config_keys()
+ 
     cfg_death = config["death"];
-    cfg_damage = config["damage"];
+    cfg_damage_done = config["damage_done"];
+    cfg_damage_taken = config["damage_taken"];
     cfg_buff = config["buff"];
+end
+
+function battle_bot_init()
+    battle_bot_validate_config()
     
     SLASH_EPGPBB1 = '/epgpbb';
+    SLASH_EPGPBB2 = '/ebb';
     
     SlashCmdList["EPGPBB"] = battle_bot_slash_handler
 end
